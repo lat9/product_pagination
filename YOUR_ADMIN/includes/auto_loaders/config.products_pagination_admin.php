471e8ea620048bb4c691dbf1ac57d218927fde9c
lat9 <lat9@vinosdefrutastropicales.com>
2016-08-10 09:53:04 -0400

    #9: Use admin auto-installer
    Remove the stand-alone installation SQL script, converting that to an
    admin-level auto-installer and adding two configuration items:  the
    version and an enable switch.

---
+++ YOUR_ADMIN/includes/auto_loaders/config.products_pagination_admin.php
@@ -0,0 +1,9 @@
+<?php
+// -----
+// Part of the "Product Pagination" plugin by lat9 (lat9@vinosdefrutastropicales.com)
+// Copyright (c) 2016 Vinos de Frutas Tropicales
+//                               
+$autoLoadConfig[200][] = array (
+    'autoType' => 'init_script',
+    'loadFile' => 'init_products_pagination_admin.php'
+);
