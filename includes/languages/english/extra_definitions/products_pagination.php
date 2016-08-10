d54d0440869f4268a2932af76e1b049160237ef6
lat9 <lat9@vinosdefrutastropicales.com>
2016-08-10 10:30:21 -0400

    #10: Update header comments
    Update the header comments of the newly-moved languages'
    extra-definitions file.

--- includes/languages/english/extra_definitions/products_pagination.php
+++ includes/languages/english/extra_definitions/products_pagination.php
@@ -1,13 +1,14 @@
 <?php
-// ----- Language defines for products_pagination add-on
-//
-define('PP_PREV_NEXT_PRODUCT', 'Currently Viewing Product ');
-define('PP_PREV_NEXT_PRODUCT_SEP', ' of ');
-define('PP_TEXT_PREVIOUS','Previous');
-define('PP_TEXT_NEXT','Next');
-define('PP_TEXT_PRODUCT_LISTING', 'View Product Listing');
-define('PP_TEXT_PRODUCT_LISTING_TITLE', 'View more &quot;%s&quot;'); // %s is replaced by the categories name
-define('PP_TEXT_PAGE', 'Page: ');
-define('PP_TEXT_ITEMS_PER_PAGE', 'Items per Page: ');
-define('PP_TEXT_ALL', 'All');
-?>
+// -----
+// Part of the "Product Pagination" plugin by lat9 (lat9@vinosdefrutastropicales.com)
+// Copyright (c) 2010-2016 Vinos de Frutas Tropicales
+// 
+define ('PP_PREV_NEXT_PRODUCT', 'Currently Viewing Product ');
+define ('PP_PREV_NEXT_PRODUCT_SEP', ' of ');
+define ('PP_TEXT_PREVIOUS', 'Previous');
+define ('PP_TEXT_NEXT', 'Next');
+define ('PP_TEXT_PRODUCT_LISTING', 'View Product Listing');
+define ('PP_TEXT_PRODUCT_LISTING_TITLE', 'View more &quot;%s&quot;'); // %s is replaced by the categories name
+define ('PP_TEXT_PAGE', 'Page: ');
+define ('PP_TEXT_ITEMS_PER_PAGE', 'Items per Page: ');
+define ('PP_TEXT_ALL', 'All');
