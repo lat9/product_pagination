857ad1b4e7b02d08c568d3f970e8c59a79da43bf
lat9 <lat9@vinosdefrutastropicales.com>
2016-08-11 14:08:52 -0400

    #10: add whitespace
    Add some whitespace to this module.

--- includes/init_includes/init_pagination.php
+++ includes/init_includes/init_pagination.php
@@ -1,36 +1,28 @@
 <?php
-/**
- * @package initSystem
- * @copyright Copyright 2003-2011 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: init_pagination.php 2012-11-20 lat9 $
- */
+// -----
+// Part of the "Product Pagination" plugin by lat9 (lat9@vinosdefrutastropicales.com)
+// Copyright (c) 2010-2016 Vinos de Frutas Tropicales
+// 
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
 
-if (PRODUCTS_PAGINATION_OTHER == 'true' && in_array($_GET['main_page'], explode(',', PRODUCTS_PAGINATION_OTHER_MAIN_PAGES))) {
-  if (isset($_POST)) {
-    if (zen_not_null($_POST['pagedrop1']) && $_POST['pagedrop1'] != $_POST['prev_pagedrop']) {
-      $_POST['pagedrop'] = $_POST['pagedrop1'];
-      
-    } elseif (zen_not_null($_POST['pagedrop2']) && $_POST['pagedrop2'] != $_POST['prev_pagedrop']) {
-      $_POST['pagedrop'] = $_POST['pagedrop2'];
-      
+if (PRODUCTS_PAGINATION_OTHER == 'true' && isset ($_GET['main_page']) && in_array ($_GET['main_page'], explode(',', PRODUCTS_PAGINATION_OTHER_MAIN_PAGES))) {
+    if (zen_not_null ($_POST['pagedrop1']) && $_POST['pagedrop1'] != $_POST['prev_pagedrop']) {
+        $_POST['pagedrop'] = $_POST['pagedrop1'];
+    } elseif (zen_not_null ($_POST['pagedrop2']) && $_POST['pagedrop2'] != $_POST['prev_pagedrop']) {
+        $_POST['pagedrop'] = $_POST['pagedrop2'];
     }
     
-    if (zen_not_null($_POST['pagecount1']) && $_POST['pagecount1'] != $_POST['prev_pagecount']) {
-      $_GET['pagecount'] = $_POST['pagecount1'];
-      
-    } elseif (zen_not_null($_POST['pagecount2']) && $_POST['pagecount2'] != $_POST['prev_pagecount']) {
-      $_GET['pagecount'] = $_POST['pagecount2'];
-      
+    if (zen_not_null ($_POST['pagecount1']) && $_POST['pagecount1'] != $_POST['prev_pagecount']) {
+        $_GET['pagecount'] = $_POST['pagecount1'];
+    } elseif (zen_not_null ($_POST['pagecount2']) && $_POST['pagecount2'] != $_POST['prev_pagecount']) {
+        $_GET['pagecount'] = $_POST['pagecount2'];  
     } elseif (zen_not_null($_POST['prev_pagecount'])) {
-      $_GET['pagecount'] = $_POST['prev_pagecount'];
+        $_GET['pagecount'] = $_POST['prev_pagecount'];
     }
     
-    if (zen_not_null($_POST['pagedrop'])) $_GET['page'] = $_POST['pagedrop'];
-  }
-
+    if (zen_not_null ($_POST['pagedrop'])) {
+        $_GET['page'] = $_POST['pagedrop'];
+    }
 }
