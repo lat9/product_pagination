51c9bc57a7dac3e9c91bae607496b23c6e873b93
lat9 <lat9@vinosdefrutastropicales.com>
2016-09-25 10:33:16 -0400

    #12: Update release dates
    ... in the database and readme.

--- YOUR_ADMIN/includes/init_includes/init_product_pagination_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_product_pagination_admin.php
@@ -5,7 +5,7 @@
 // Starting with v2.0.0 of the plugin, perform the auto-install of the various configuration items.
 //
 define ('PRODUCTS_PAGINATION_VERSION_CURRENT', '2.0.0');
-define ('PRODUCTS_PAGINATION_VERSION_CURRENT_DATE', '09-24-2016');
+define ('PRODUCTS_PAGINATION_VERSION_CURRENT_DATE', '09-25-2016');
 
 $pp_current_version = PRODUCTS_PAGINATION_VERSION_CURRENT . ' (' . PRODUCTS_PAGINATION_VERSION_CURRENT_DATE . ')';
 
