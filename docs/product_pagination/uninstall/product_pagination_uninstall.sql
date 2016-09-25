9bd112491e8b50d5ca796ac847303a2f90401750
lat9 <lat9@vinosdefrutastropicales.com>
2016-09-25 10:28:36 -0400

    #12: Uninstall reorganization
    Update the uninstall script so that it doesn't rely on the
    configuration_group_id.

--- docs/product_pagination/uninstall/product_pagination_uninstall.sql
+++ docs/product_pagination/uninstall/product_pagination_uninstall.sql
@@ -1,5 +1,3 @@
-SELECT @cgi:=configuration_group_id
-FROM configuration_group WHERE configuration_group_title="Products Pagination";
-DELETE FROM configuration WHERE configuration_group_id=@cgi AND configuration_group_id != 0;
-DELETE FROM configuration_group WHERE configuration_group_id=@cgi AND configuration_group_id != 0;
-DELETE FROM admin_pages WHERE page_key='configProdPagination';
+DELETE FROM configuration WHERE configuration_key LIKE 'PRODUCTS_PAGINATION_%';
+DELETE FROM configuration_group WHERE configuration_group_title = 'Products Pagination' LIMIT 1;
+DELETE FROM admin_pages WHERE page_key='configProdPagination' LIMIT 1;
