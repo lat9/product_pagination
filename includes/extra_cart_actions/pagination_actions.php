6f174d36e15d12514e6f79d992e6dc267e507195
lat9 <lat9@vinosdefrutastropicales.com>
2024-04-21 10:37:57 -0400

    Catch up readme, refactor extra-cart-action

--- includes/extra_cart_actions/pagination_actions.php
+++ includes/extra_cart_actions/pagination_actions.php
@@ -1,7 +1,9 @@
 <?php
 // -----
 // Part of the "Product Pagination" plugin, provided by lat9 (lat9@vinosdefrutastropicales.com)
-// Copyright (C) 2010-2016, Vinos de Frutas Tropicales.
+// Copyright (C) 2010-2024, Vinos de Frutas Tropicales.
+//
+// Last updated: v3.0.0
 //
 if (!defined('IS_ADMIN_FLAG')) {
     die ('Illegal Access');
@@ -11,10 +13,10 @@ if (!defined('IS_ADMIN_FLAG')) {
 // If there's a multiple-products-add-to-cart action to be acted upon and the plugin's handling has been configured to
 // present a page- and/or pagecount-dropdown, need to provide some fixups for the follow-on processing.
 //
-if (isset ($_GET['action']) && $_GET['action'] == 'multiple_products_add_product' && (PRODUCTS_PAGINATION_DISPLAY_PAGEDROP == 'true' || PRODUCTS_PAGINATION_PRODUCT_COUNT == 'true') ) {
-    if (isset ($_POST['pp_which_input'])) {
-        $input_info = explode ('-', $_POST['pp_which_input']);
-        if (count ($input_info) == 2) {
+if ((PRODUCTS_PAGINATION_DISPLAY_PAGEDROP === 'true' || PRODUCTS_PAGINATION_PRODUCT_COUNT === 'true') && isset($_GET['action']) && $_GET['action'] === 'multiple_products_add_product') {
+    if (isset($_POST['pp_which_input'])) {
+        $input_info = explode('-', $_POST['pp_which_input']);
+        if (count($input_info) === 2) {
             switch ($input_info[0]) {
                 case 'p':
                     $base_varname = 'page';
@@ -26,12 +28,12 @@ if (isset ($_GET['action']) && $_GET['action'] == 'multiple_products_add_product
                     $base_varname = '';
                     break;
             }
-            if ($base_varname != '') {
+            if ($base_varname !== '') {
                 $posted_varname = 'pp_' . $base_varname . $input_info[1];
-                if (isset ($_POST[$posted_varname])) {
+                if (isset($_POST[$posted_varname])) {
                     $_GET[$base_varname] = $_POST[$posted_varname];
-                    unset ($_GET['action']);
-                    zen_redirect (zen_href_link ($_GET['main_page'], zen_get_all_get_params ()));
+                    unset($_GET['action']);
+                    zen_redirect(zen_href_link($_GET['main_page'], zen_get_all_get_params()));
                 }
             }
         }
